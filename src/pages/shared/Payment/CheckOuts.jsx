a5b66a98a4300b4cb045535e20024ccb7f1e17cf
iqbalhossain4 <iqbal-hossain@mail.com>
2023-06-14 00:15:43 +0600

    add footer

--- src/pages/shared/Payment/CheckOuts.jsx
+++ src/pages/shared/Payment/CheckOuts.jsx
@@ -1,41 +1,48 @@
-// const CheckOuts = () => {
-// const stripe = useStripe();
-//   const elements = useElements();
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     if (!stripe || !elements) {
-//       return;
-//     }
-//     const card = elements.getElement(CardElement);
-//     if (card === null) {
-//       return;
-//     }
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <CardElement
-//           options={{
-//             style: {
-//               base: {
-//                 fontSize: "16px",
-//                 color: "#424770",
-//                 "::placeholder": {
-//                   color: "#aab7c4",
-//                 },
-//               },
-//               invalid: {
-//                 color: "#9e2146",
-//               },
-//             },
-//           }}
-//         />
-//         <button type="submit" disabled={!stripe}>
-//           Pay
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
+import React from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
-// export default CheckOuts;
+const CheckOuts = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-warning mt-8"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckOuts;
--- src/pages/shared/Payment/Payment.jsx
+++ src/pages/shared/Payment/Payment.jsx
@@ -1,12 +1,17 @@
-// import { loadStripe } from "@stripe/stripe-js";
-// import CheckOuts from "./CheckOuts";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckOuts from "./CheckOuts";
+import { Elements } from "@stripe/react-stripe-js";
+import HeaderSection from "../../../Component/Header/HeaderSection";
 
-// const stripePromise = loadStripe();
+// provide publish key
+const stripePromise = loadStripe(import.meta.env.VITE_payment_gatway);
 const Payment = () => {
   return (
-    <div className="font-serif">
-      {/* <CheckOuts></CheckOuts> */}
-      hello
+    <div className="font-serif bg-white">
+      <HeaderSection text="Payment"></HeaderSection>
+      <Elements stripe={stripePromise}>
+        <CheckOuts></CheckOuts>
+      </Elements>
     </div>
   );
 };
