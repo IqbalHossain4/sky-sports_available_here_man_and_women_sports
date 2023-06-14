d68f229ea4d170c109e888118d5ce05bfb86d2a2
iqbalhossain4 <iqbal-hossain@mail.com>
2023-06-14 08:34:22 +0600

    add class

--- src/pages/shared/Payment/Payment.jsx
+++ src/pages/shared/Payment/Payment.jsx
@@ -2,10 +2,11 @@ import { loadStripe } from "@stripe/stripe-js";
 import CheckOuts from "./CheckOuts";
 import { Elements } from "@stripe/react-stripe-js";
 import HeaderSection from "../../../Component/Header/HeaderSection";
-
+import useCourse from "../../../Hook/useCourse";
 // provide publish key
 const stripePromise = loadStripe(import.meta.env.VITE_payment_gatway);
 const Payment = () => {
+  const [cart] = useCourse();
   return (
     <div className="font-serif bg-white">
       <HeaderSection text="Payment"></HeaderSection>
